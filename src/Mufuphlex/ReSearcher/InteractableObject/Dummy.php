d35e1fc264f951cdf17ca9bf9f68caf5f30495ff
AlexMiroshnikov <ilex@nigma.ru>
2015-08-09 21:11:06 +0700

    ReSearcher module -Commit forgotten Dummy Interactable object

---
+++ src/Mufuphlex/ReSearcher/InteractableObject/Dummy.php
@@ -0,0 +1,48 @@
+<?php
+namespace Mufuphlex\ReSearcher\InteractableObject;
+
+/**
+ * Class Dummy
+ * @package Mufuphlex\ReSearcher\InteractableObject
+ */
+class Dummy extends \Mufuphlex\ReSearcher\InteractableObject
+{
+	protected $_id = 'dummy id';
+	protected $_type = 'dummy type';
+	protected $_tokens = array('dummy', 'token');
+	protected $_mutable = false;
+
+	public function __construct(array $options = array())
+	{
+		if (!empty($options['id']))
+		{
+			$this->_id = (string)$options['id'];
+		}
+
+		if (!empty($options['mutable']))
+		{
+			$this->_mutable = (bool)$options['mutable'];
+		}
+	}
+
+	/**
+	 * @param $source
+	 * @return $this
+	 */
+	public static function create($source)
+	{
+		return new self();
+	}
+
+	/**
+	 * @param array $resultsIds
+	 * @return mixed
+	 */
+	public static function createResults(array $resultsIds)
+	{
+		foreach ($resultsIds as $id)
+		{
+			return array('result' => 'dummy result', 'id' => $id);
+		}
+	}
+}
