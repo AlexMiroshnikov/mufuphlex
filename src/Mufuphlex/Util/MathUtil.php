4b77eeaf32bc99de7a57d58535deb4c78b63b07f
AlexMiroshnikov <ilex@ilex-thinknote>
2015-10-02 07:58:13 +0700

    Add MathUtil

---
+++ src/Mufuphlex/Util/MathUtil.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilex
+ * Date: 10/2/15
+ * Time: 7:35 AM
+ */
+
+namespace Mufuphlex\Util;
+
+
+class MathUtil
+{
+
+}
---
+++ src/Mufuphlex/Util/Tests/MathUtilTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ilex
+ * Date: 10/2/15
+ * Time: 7:45 AM
+ */
+class MathUtilTest extends PHPUnit_Framework_TestCase
+{
+
+}
