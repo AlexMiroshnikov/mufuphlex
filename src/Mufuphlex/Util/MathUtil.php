701b315135b2d69548a52892203fa3c6d7b26c2e
AlexMiroshnikov <ilex@ilex-thinknote>
2015-10-02 08:38:34 +0700

    Add mult

--- src/Mufuphlex/Util/MathUtil.php
+++ src/Mufuphlex/Util/MathUtil.php
@@ -1,15 +1,133 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ilex
- * Date: 10/2/15
- * Time: 7:35 AM
- */
-
 namespace Mufuphlex\Util;
 
-
+/**
+ * Class MathUtil
+ * @package Mufuphlex\Util
+ */
 class MathUtil
 {
+    /**
+     * @param int|string $a
+     * @param int|string $b
+     * @return string
+     */
+    public static function sum($a, $b)
+    {
+        $a = static::_convertToArray($a);
+        $b = static::_convertToArray($b);
+        $cnt = max(count($a), count($b));
+        $result = array();
+        $mind = 0;
 
+        for ($i = 0; $i < $cnt; $i++)
+        {
+            $sum = (isset($a[$i]) ? $a[$i] : 0);
+
+            if (isset($b[$i]))
+            {
+                $sum += $b[$i];
+            }
+
+            if ($mind)
+            {
+                $sum += $mind;
+                $mind = 0;
+            }
+
+            if ($sum >= 10)
+            {
+                $mind = 1;
+                $sum = $sum - 10;
+            }
+
+            $result[$i] = $sum;
+        }
+
+        if ($mind)
+        {
+            $result[] = $mind;
+        }
+
+        return implode('', array_reverse($result));
+    }
+
+    /**
+     * @param mixed $a
+     * @param midex $b
+     * @return string
+     */
+    public static function mult($a, $b)
+    {
+        $vals = array(static::_convertToArray($a), static::_convertToArray($b));
+        usort($vals, function($a, $b){
+            if (count($a) > count($b)) return -1;
+            return 0;
+        });
+        $cnt = count($vals[0]);
+        $result = 0;
+        $mind = 0;
+
+        for ($i = 0; $i < $cnt; $i++)
+        {
+            $line = array();
+            $j = 0;
+
+            while (isset($vals[1][$j]))
+            {
+                $mul = $vals[0][$i] * $vals[1][$j];
+
+                if ($mind > 0)
+                {
+                    $mul += $mind;
+                    $mind = 0;
+                }
+
+                if ($mul >= 10)
+                {
+                    $mind = floor($mul / 10);
+                    $mul = $mul % 10;
+                }
+
+                $line[] = $mul;
+                $j++;
+            }
+
+            if ($mind > 0)
+            {
+                $line[] = $mind;
+                $mind = 0;
+            }
+
+            $line = array_reverse($line);
+
+            for ($k=0; $k < $i; $k++)
+            {
+                $line[] = 0;
+            }
+
+            $result = static::sum($result, implode('', $line));
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param mixed $val
+     * @return array
+     */
+    private static function _convertToArray($val)
+    {
+        $val = (string)$val;
+        $i = 0;
+        $arr = array_fill(0, strlen($val), 0);
+
+        while (isset($val[$i]))
+        {
+            $arr[$i] = $val[$i];
+            $i++;
+        }
+
+        return array_reverse($arr);
+    }
 }
--- src/Mufuphlex/Util/Tests/MathUtilTest.php
+++ src/Mufuphlex/Util/Tests/MathUtilTest.php
@@ -1,12 +1,55 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: ilex
- * Date: 10/2/15
- * Time: 7:45 AM
- */
 class MathUtilTest extends PHPUnit_Framework_TestCase
 {
+    public function testSum()
+    {
+        $sums = array(
+            array(0,0,0),
+            array(1,0,1),
+            array(0,1,1),
+            array(1,2,3),
+            array(15,2,17),
+            array(3,19,22),
+            array(235,899,1134),
+            array(
+                implode('', array_fill(0, 400, 1)),
+                implode('', array_fill(0, 400, 2)),
+                implode('', array_fill(0, 400, 3))
+            )
+        );
 
+        foreach ($sums as $data)
+        {
+            $this->assertEquals((string)$data[2], \Mufuphlex\Util\MathUtil::sum($data[0], $data[1]));
+        }
+    }
+
+    public function testMult()
+    {
+        $mults = array(
+            array(0,0,0),
+            array(1,0,0),
+            array(0,1,0),
+            array(1,2,2),
+            array(2,1,2),
+            array(15,2,30),
+            array(3,16,48),
+            array(
+                53,
+                '80658175170943878571660636856403766975289505440883277824000000000000',
+                '4274883284060025564298013753389399649690343788366813724672000000000000'
+            ),
+            array(
+                '230843697339241380472092742683027581083278564571807941132288000000000000',
+                55,
+                '12696403353658275925965100847566516959580321051449436762275840000000000000'
+            )
+        );
+
+        foreach ($mults as $data)
+        {
+            $this->assertEquals((string)$data[2], \Mufuphlex\Util\MathUtil::mult($data[0], $data[1]));
+        }
+    }
 }
